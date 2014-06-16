c16d552617c16d267c1e56adde20bf148bd053aa
tiago <tiago.avila09@gmail.com>
2014-06-16 15:19:30 -0300

    comitando arquivos

---
+++ Teste_MetaDataType/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Teste_MetaDataType.Models
+{
+    public partial class User
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ Teste_MetaDataType/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Teste_MetaDataType.Models
+{
+    [MetadataType(typeof(UserModel))]
+    public partial class User
+    {
+        class UserModel
+        {
+            [Required]
+            public string Nome { get; set; }
+
+            [Required]
+            public string Senha { get; set; }
+        }
+    }
+}
